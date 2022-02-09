d2108733a81fa792bdd0c2efd1fe393b05004721
amiribnjaman <amiribnjaman@gmail.com>
2022-02-09 13:47:04 +0600

    something edited

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 */
 
 
-// Dom selector 
+// Dom selection 
 let countShow = document.getElementById('count');
 let addBtn = document.getElementById('add-btn');
 let minusBtn = document.getElementById('minus-btn');
