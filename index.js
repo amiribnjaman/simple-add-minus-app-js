392ce02997641a417dcf7740dcab147d09c58fc4
amiribnjaman <amiribnjaman@gmail.com>
2022-02-09 13:34:57 +0600

    add functionality done

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 // Dom selector 
-let countShow = document.getElementById('count')
-let addBtn = document.getElementById('add-btn')
-let minusBtn = document.getElementById('minus-btn')
+let countShow = document.getElementById('count');
+let addBtn = document.getElementById('add-btn');
+let minusBtn = document.getElementById('minus-btn');
+
+let count = 0
+
+// event listener
+addBtn.addEventListener('click', function(e){
+    count++;
+    countShow.innerText = count
+})
