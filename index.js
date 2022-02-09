30412eb839a4a529c2029499a63d24984b525f5b
amiribnjaman <amiribnjaman@gmail.com>
2022-02-09 13:40:22 +0600

    minus functionality done

--- index.js
+++ index.js
@@ -3,10 +3,28 @@ let countShow = document.getElementById('count');
 let addBtn = document.getElementById('add-btn');
 let minusBtn = document.getElementById('minus-btn');
 
+let newHeading = document.createElement('h3')
 let count = 0
 
+
+// counting show
+show()
+function show() {
+    newHeading.innerText = count
+    countShow.appendChild(newHeading)
+}
+
 // event listener
-addBtn.addEventListener('click', function(e){
+// add functionality
+addBtn.addEventListener('click', function () {
     count++;
-    countShow.innerText = count
+    show();
+})
+
+// minus functionality
+minusBtn.addEventListener('click', function(){
+    if(count > 0){
+        count--;
+        show()
+    }
 })
